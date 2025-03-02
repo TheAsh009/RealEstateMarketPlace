6c5d456b59f7c4e6bcc8e272390ce639e808584b
Ashitosh Patil <Ashitoshpatil27@gmail.com>
2025-03-01 18:05:40 -0800

    complete img upload

--- client/src/assets/pages/Profile.jsx
+++ client/src/assets/pages/Profile.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
 
 function Profile() {
   const { currentUser } = useSelector((state) => state.user);
-
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(null);
+  console.log(file);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-8">Profile</h1>
       <form className="flex flex-col gap-3">
+        <input
+          type="file"
+          onChange={(e) => setFile(e.target.files[0])}
+          ref={fileRef}
+          accept="image/*"
+          hidden
+        />
         <img
           src={currentUser.avatar}
           alt="Profile"
+          onClick={() => fileRef.current.click()}
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 "
         />
         <input
